689a5a68b8b9a298fede403dc13f278754dad003
Mateusz Olszewski <olszewski.mateu@gmail.com>
2024-10-12 23:53:02 +0200

    init

---
+++ src/test/java/com/molszewski/demos/poker/PokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.molszewski.demos.poker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
