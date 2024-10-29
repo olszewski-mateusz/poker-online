a19c492e4bd39c64a893204942e90ac140f8e778
Mateusz Olszewski <olszewski.mateu@gmail.com>
2024-10-29 19:35:06 +0100

    add google material symbols

--- frontend/poker/src/app/app.component.ts
+++ frontend/poker/src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {CardComponent, CardSize} from "./components/game-page/card/card.component";
 import {Rank} from './model/rank';
 import {Suit} from './model/suit';
+import {MatIconRegistry} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
@@ -12,4 +13,8 @@ import {Suit} from './model/suit';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  private readonly matIconReg: MatIconRegistry = inject(MatIconRegistry);
+  constructor() {
+    this.matIconReg.setDefaultFontSetClass('material-symbols-outlined');
+  }
 }
