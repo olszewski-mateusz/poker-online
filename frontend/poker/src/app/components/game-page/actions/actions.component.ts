502079dff90ad581389ba47edc42c561a8d8aa1f
Mateusz Olszewski <olszewski.mateu@gmail.com>
2024-12-12 19:59:40 +0100

    add basic actions component impl

--- frontend/poker/src/app/components/game-page/actions/actions.component.ts
+++ frontend/poker/src/app/components/game-page/actions/actions.component.ts
@@ -1,32 +1,107 @@
-import {ChangeDetectionStrategy, Component, computed, inject, input, InputSignal} from '@angular/core';
-import {Game} from '../../../model/game';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed, effect,
+  inject,
+  input,
+  InputSignal, model, ModelSignal,
+  signal,
+  Signal,
+  WritableSignal
+} from '@angular/core';
+import {Game, GameState, Player} from '../../../model/game';
 import {MatButton} from '@angular/material/button';
 import {ApiService} from '../../../services/api.service';
+import {MatSlider, MatSliderThumb} from '@angular/material/slider';
+import {MatFormField, MatLabel} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
 
 @Component({
   selector: 'app-actions',
   standalone: true,
   imports: [
-    MatButton
+    MatButton,
+    MatSlider,
+    MatSliderThumb,
+    MatFormField,
+    MatInput,
+    MatLabel
   ],
   templateUrl: './actions.component.html',
   styleUrl: './actions.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ActionsComponent {
+  protected readonly GameState: typeof GameState = GameState;
+
   game: InputSignal<Game> = input.required<Game>();
 
-  myPlayer = computed(() => {
+  amount: ModelSignal<number> = model<number>(0);
+
+  private readonly apiService: ApiService = inject(ApiService);
+
+  protected readonly myPlayer: Signal<Player | undefined> = computed(() => {
     const game: Game = this.game();
     return game.players.find(value => value.id === game.myId);
   });
 
-  private readonly apiService: ApiService = inject(ApiService);
+  protected readonly myTurn: Signal<boolean> = computed(() => {
+    const game: Game = this.game();
+    return game.currentPlayerId === game.myId;
+  })
 
-  sendReady(game: Game) {
+  protected readonly raiseSelected: WritableSignal<boolean> = signal<boolean>(false);
+
+  protected readonly isBet: Signal<boolean> = computed(() => {
+    const game: Game = this.game();
+    return game.players.some(p => {
+      return p.bet > game.configuration.ante;
+    })
+  })
+
+  protected readonly minBet: Signal<number> = computed(() => {
+    return 2 * this.game().players.map(p => p.bet)
+      .reduce((a, b) => Math.max(a, b), 0);
+  })
+
+  protected readonly maxBet: Signal<number> = computed(() => {
+    return this.myPlayer()?.money ?? this.minBet();
+  })
+
+  constructor() {
+    effect(() => {
+      const minBet: number = this.minBet();
+      this.amount.update(value => Math.max(minBet, value));
+    }, {allowSignalWrites: true});
+  }
+
+  sendReady(): void {
+    const game: Game = this.game();
     const myPlayer = this.myPlayer();
     if (myPlayer) {
-      this.apiService.readyAction(game.gameId, game.myId, !myPlayer.ready).subscribe();
+      this.apiService.sendReady(game.gameId, game.myId, !myPlayer.ready).subscribe();
     }
+  }
+
+  sendCheckOrCall(): void {
+    const game: Game = this.game();
+    this.apiService.sendCheckOrCall(game.gameId, game.myId).subscribe();
+  }
+
+  sendBetOrRaise(): void {
+    const game: Game = this.game();
+    this.apiService.sendBetOrRaise(game.gameId, game.myId, this.amount()).subscribe();
+    this.raiseSelected.set(false);
+  }
+
+  sendAllIn(): void {
+    const game: Game = this.game();
+    this.apiService.sendAllIn(game.gameId, game.myId).subscribe();
+    this.raiseSelected.set(false);
+  }
+
+  sendFold(): void {
+    const game: Game = this.game();
+    this.apiService.sendFold(game.gameId, game.myId).subscribe();
   }
 }
